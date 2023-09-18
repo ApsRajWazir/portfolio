7f463c9ba43ed9c0ac7deca872e343acb88c2d9d
aps <amarpreet888singh@gmail.com>
2023-09-18 16:55:52 +0530

    4th Commit - Created About Page of Portfolio website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,19 @@
 const About = () => {
   return (
     <main id="about">
-      <center>ABOUT</center>
+      <article>
+        Hey I am Amarpreet Singh, Full Stack Developer.
+      </article>
+      <article>
+        I made various projects in HTML, CSS, Js, Reactjs & Nextjs
+      </article> 
+      <div>
+      <span>
+        Email: amarpreet888singh@gmail.com
+      </span>
+      <span>Place: Jammu, India</span>
+      </div>
+
     </main>
   )
 }
